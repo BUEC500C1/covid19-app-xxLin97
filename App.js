2802f5002cf4b6fe714bbc5cbaa80f0739110a9e
Zhichao_Lu <55032729+xxLin97@users.noreply.github.com>
2021-01-05 16:50:35 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,263 @@
-import React; 
-import {Text, View} from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View , TextInput} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+ 
+ 
+const styles = StyleSheet.create({
+  container: {
+    position:'absolute',
+    top:0,
+    left:0,
+    right:0,
+    bottom:0,
+    justifyContent: 'flex-end',
+  },
+  map: {
+    position:'absolute',
+    top:0,
+    left:0,
+    right:0,
+    bottom:0,
+  },
+}); 
 
-function mainsc() {
-  return (
-    <View style = {{flew: 1. justifyContent: 'center', alignItems: 'center'}}>
-      <Text> This is a Global Covid-19 cases map <Text>
-    <View>
-    );
-}
-
-const Bottom_tab= createBottomtabNavigator();
-
-export class App extends Globalmap {
-  render(){
+export default class App extends React.Component {
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+  render() {
+  var mapStyle = [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#ebe3cd"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#523735"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#f5f1e6"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#c9b2a6"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#dcd2be"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#ae9e90"
+        }
+      ]
+    },
+    {
+      "featureType": "landscape.natural",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#dfd2ae"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#dfd2ae"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#93817c"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#a5b076"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#447530"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#f5f1e6"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#fdfcf8"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#f8c967"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#e9bc62"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway.controlled_access",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#e98d58"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway.controlled_access",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#db8555"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#806b63"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.line",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#dfd2ae"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.line",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#8f7d77"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.line",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#ebe3cd"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.station",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#dfd2ae"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#b9d3c2"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#92998d"
+        }
+      ]
+    }
+  ]
+  
     return (
-      <Navigationcontainer>
-        <Bottom_tab.Navigator>
-          <Bottom_tab.Sc name = "Home" mode = mainsc>
-          <Bottom_tab.Sc name = "Map" mode = Globalmap>
-        <Bottom_tab.Navigator>
-      <Navigationcontainer>
-      )
+      <View style={styles.container}>
+        
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 42.370171,
+            longitude: -71.030312,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.50,
+          }}
+          customMapStyle={mapStyle}
+        >
+       
+        </MapView>
+      </View>
+    );
   }
 }
-
